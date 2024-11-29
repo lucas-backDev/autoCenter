454bcda300e56be186a995a6ba8e10f759f7320f
lucas-backDev <lucas.cgomesb19@gmail.com>
2024-11-29 10:03:24 -0300

    criando uma função no clientes.js que cria campos para preencher informações dos carros (criar um novo input) e estruturando banco de dados SQLite

--- clientes/models.py
+++ clientes/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
 # Create your models here.
+class Cliente(models.Model):
+  nome = models.CharField(max_length=128)
+  sobrenome = models.CharField(max_length=128)
+  email = models.EmailField(max_length=128)
+  cpf = models.CharField(max_length=11)
+
+  def __str__(self) -> str:
+    return self.nome
+  
+class Carro(models.Model):
+  carro = models.CharField(max_length=128)
+  placa = models.CharField(max_length=7)
+  ano = models.IntegerField()
+  cliente = models.ForeignKey(Cliente, on_delete=models.CASCADE)
+  lavagens = models.IntegerField(default=0)
+  consertos = models.IntegerField(default=0)
+
+  def __str__(self) -> str:
+    return self.carro
+  
